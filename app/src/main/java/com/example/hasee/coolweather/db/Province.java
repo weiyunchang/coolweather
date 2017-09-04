a64cb13df42b466eac78cf16acafd42298309ee9
weiyunchang <1018630954@qq.com>
2017-09-04 11:31:39 +0800

    完成三级列表

--- app/src/main/java/com/example/hasee/coolweather/db/Province.java
+++ app/src/main/java/com/example/hasee/coolweather/db/Province.java
@@ -8,7 +8,7 @@ import org.litepal.crud.DataSupport;
 
 public class Province extends DataSupport{
     private int id;
-    private String proviceName;
+    private String provinceName;
     private int provinceCode;
 
     public int getId() {
@@ -19,12 +19,12 @@ public class Province extends DataSupport{
         this.id = id;
     }
 
-    public String getProviceName() {
-        return proviceName;
+    public String getProvinceName() {
+        return provinceName;
     }
 
-    public void setProviceName(String proviceName) {
-        this.proviceName = proviceName;
+    public void setProvinceName(String proviceName) {
+        this.provinceName = proviceName;
     }
 
     public int getProvinceCode() {
